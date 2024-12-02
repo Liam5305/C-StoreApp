a5eb73d37163c36c2e4a2bfda4c36e855ad5d90d
Liam Rutherford <liamrutherford10@gmail.com>
2024-12-02 10:02:47 +0000

    Js Functions

--- StoreApp/wwwroot/js/site.js
+++ StoreApp/wwwroot/js/site.js
@@ -1,32 +1,59 @@
 ﻿function DisplayDateOnHome() {
-    const today = new Date();
-    document.getElementById('currentDate').textContent = today.toLocaleDateString();
+    const dateElement = document.getElementById('currentDate');
+    if (dateElement) {
+        const today = new Date();
+        dateElement.textContent = today.toLocaleDateString();
+    }
 }
 
-function addRotationEffect(selector) {
-    const element = document.querySelector(selector);
-    console.log('Found element', element);
-    if (element) {
-        element.style.cssText = `
+function addRotationEffect(...selectors) {
+    selectors.forEach(selector => {
+        const element = document.querySelector(selector);
+        console.log('Found element', element);
+        if (element) {
+            element.style.cssText = `
             transition: transform 2s ease;
             transform-origin: center center;
             display: inline-block;
             transform: rotate(0deg);
         `;
 
+            element.addEventListener('mouseenter', () => {
+                console.log('Mouse enter')
+                element.style.transform = 'rotate(360deg)';
+            });
+
+            element.addEventListener('mouseleave', () => {
+                console.log('Mouse leave')
+                element.style.transform = 'rotate(0deg)';
+            });
+        }
+    })
+}
+
+function addEnlargeEffect(selector) {
+    const element = document.querySelector(selector)
+    console.log('element found', element);
+    if (element) {
+        element.style.cssText = `
+        transition: transform 2s ease;
+        transform: scale(1);
+        `;
+
         element.addEventListener('mouseenter', () => {
-            console.log('Mouse enter')
-            element.style.transform = 'rotate(360deg)';
+            console.log('mouseenter')
+            element.style.transform = 'scale(1.2)';
         });
 
         element.addEventListener('mouseleave', () => {
-            console.log('Mouse leave')
-            element.style.transform = 'rotate(0deg)';
+            console.log('mouseleave')
+            element.style.transform = 'scale(1)';
         });
     }
 }
 
 window.onload = () => {
     DisplayDateOnHome();
-    addRotationEffect('.main-content h1');
+    addRotationEffect('.main-content h1', '.admin-create-item p');
+    addEnlargeEffect('.article-content p');
 };
